7fbb4c69552f570b0996a9319177901591403bbc
Lena <olena.ratushna21@gmail.com>
2021-09-04 20:02:41 +0300

    Added cart and sing-in modal popup

--- js/script.js
+++ js/script.js
@@ -5,4 +5,18 @@ $(function(){
       $(this).toggleClass('open');
       $(this).next('.category__list').toggleClass('open');
    });
+
+   $('.header__cart, .cart__title, .cart__button').click(function(){
+       $('.cart').toggleClass('open');
+   });
+
+   $('.header__sign-in').click(function(){
+       $('.black-fill,.modal').toggleClass('open');
+       $('body').css('overflow', 'hidden');
+   });
+
+   $('.modal__close, .black-fill').click(function(){
+       $('.black-fill,.modal').toggleClass('open');
+       $('body').css('overflow', '');
+   });
 });
